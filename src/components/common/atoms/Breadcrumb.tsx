571f11a32c6693232caac452303b46b18fe09427
Pramesh Karki <prameshkarki0407@gmail.com>
2023-05-02 22:17:17 +0545

    add: breadcrumb

---
+++ src/components/common/atoms/Breadcrumb.tsx
@@ -0,0 +1,38 @@
+import {
+  Box,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  Breadcrumb as ChakraBreadcrumb,
+} from "@chakra-ui/react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { AiOutlineHome } from "react-icons/ai";
+
+const Breadcrumb = () => {
+  const router = useRouter();
+  const pathSegments = router.asPath
+    .split("/")
+    .filter((segment) => segment !== "");
+
+  return (
+    <Box bg={"white"} px={20} py={1}>
+      <ChakraBreadcrumb>
+        <BreadcrumbItem>
+          <Link href="/">
+            <AiOutlineHome size={20} />
+          </Link>
+        </BreadcrumbItem>
+        {pathSegments.map((segment, index) => {
+          const path = `/${pathSegments.slice(0, index + 1).join("/")}`;
+          return (
+            <BreadcrumbItem key={path}>
+              <BreadcrumbLink href={path}>{segment}</BreadcrumbLink>
+            </BreadcrumbItem>
+          );
+        })}
+      </ChakraBreadcrumb>
+    </Box>
+  );
+};
+
+export default Breadcrumb;
--- src/layouts/Main.tsx
+++ src/layouts/Main.tsx
@@ -1,7 +1,11 @@
+import { useRouter } from "next/router";
 import { ReactNode } from "react";
+import Breadcrumb from "../components/common/atoms/Breadcrumb";
 import Footer from "../components/common/molecules/Footer";
 import MenuBar from "../components/common/molecules/MenuBar";
 import NavigationBar from "../components/common/molecules/NavigationBar";
+
+const ROOT_PATH = "/";
 
 interface IProps {
   children: ReactNode;
@@ -9,11 +13,14 @@ interface IProps {
 
 const Main = (props: IProps) => {
   const { children } = props;
+  const router = useRouter();
+  const currentPath = router.pathname;
   return (
     <>
       <header className="sticky top-0 z-50">
         <NavigationBar />
         <MenuBar />
+        {currentPath !== ROOT_PATH && <Breadcrumb />}
       </header>
       <main>{children}</main>
       <Footer />
